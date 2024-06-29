663e3218a91e10f4561f793084ca0b9f4a27aa4f
fazaamal <fazasophian123@gmail.com>
2024-06-29 12:01:50 +0800

    Initial Commit

---
+++ CaptureTheFlag/CaptureTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureTheFlagApp.swift
+//  CaptureTheFlag
+//
+//  Created by Faza Amal Sophian on 29/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CaptureTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
